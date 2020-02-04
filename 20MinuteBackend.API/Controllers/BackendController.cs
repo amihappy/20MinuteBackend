3ef09c3892a77cd369e6e6d057d58c69a832c3cd
Jacek <jacek.gratka@gmail.com>
2020-02-04 22:07:04 +0100

    Backend Controller

---
+++ 20MinuteBackend.API/Controllers/BackendController.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace _20MinuteBackend.API.Controllers
+{
+    [Route("[controller]")]
+    public class BackendController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Create")]
+        public async Task<IActionResult> Create()
+        {
+            string jsonPayload = string.Empty;
+            using (var reader = new StreamReader(this.HttpContext.Request.Body))
+            {
+                jsonPayload = await reader.ReadToEndAsync();
+            }
+            try
+            {
+                var json = JObject.Parse(jsonPayload);
+                return Ok(json.ToString());
+            }
+            catch (JsonReaderException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("{guid}")]
+        public async Task<IActionResult> Get(string guid)
+        {
+            return Ok(guid);
+        }
+    }
+}
